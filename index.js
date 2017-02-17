204faf9bac47121e597727a437b574cd8207b976
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-17 15:45:11 +0100

    Implements feed

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const { parse } = require('url')
 const { send } = require('micro')
 const config = require('./config')
 const getData = require('./lib/get-data')
+const generateFeed = require('./lib/generate-feed')
 
 module.exports = async (request, response) => {
   const { pathname } = await parse(request.url, true)
   if (pathname === '/rss') {
     const json = await getData({url: config.RECRUITMENTS_URL, type: 'json'})
-    send(response, 200, json)
+    const rss = generateFeed(json)
+    response.setHeader('Content-Type', 'application/xml')
+    send(response, 200, rss)
   } else {
     response.setHeader('Content-Type', 'text/html')
     const readme = readFileSync('./README.md', 'utf-8')
